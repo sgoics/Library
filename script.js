c61c63a9815bff509e10ab1b7acb552c6b7db306
Seth Goicoechea (sgoicoechea) <sgoicoechea@micron.com>
2022-07-11 10:26:44 -0600

    second commit

--- script.js
+++ script.js
@@ -42,8 +42,8 @@ const library = new Library()
 
 //User Interface
 
-const accountBtn = document.getElementbyId  ('accountBtn')
-const accountModal = document.getElementById('accoutModal')
+const accountBtn = document.getElementById  ('accountBtn')
+const accountModal = document.getElementById('accountModal')
 const addBookBtn = document.getElementById  ('addBookBtn')
 const addBookModal = document.getElementById('addBookModal')
 const errorMsg = document.getElementById    ('errorMsg')
@@ -57,11 +57,209 @@ const loadingRing = document.getElementById ('loadingRing')
 const setupNavbar = (user) => {
     if (user){
         loggedIn.classList.add('active')
-        loggedOut.classList.remove(active)
+        loggedOut.classList.remove('active')
     }else{
         loggedIn.classList.remove('active')
-        loggedOut.classList.remove('active')
+        loggedOut.classList.add('active')
     }
     loadingRing.classList.remove('active')
 }
 
+const setupAccoutModal = (user) => {
+    if (user) {
+        accountModal.innerHTML = `
+        <p>Logged in as</p>
+        <p><strong>${user.email.split('@')[0]}</strong></p>`
+    }else{
+        accountModal.innerHtml = ''
+    }
+}
+
+const openAddBookModal = () => {
+    addBookForm.reset()
+    addBookModal.classList.add  ('active')
+    overlay.classList.add       ('active')
+}
+
+const closeAddBookModal = () => {
+    addBookModal.classList.remove ('active')
+    overlay.classList.remove      ('active')
+}
+
+const openAccountModal = () => {
+    accountModal.classList.add    ('active')
+    overlay.classList.add         ('active')       
+}
+
+const closeAccountModal = () => {
+    accountModal.classList.remove ('active')
+    overlay.classList.remove      ('active')
+}
+
+const closeAllModals = () => {
+    closeAddBookModal()
+    closeAccountModal()
+}
+
+const handleKeyboardInput = (e) => {
+    if (e.key === 'Escape') closeAllModals()
+}
+
+const updateBooksGrid = () => {
+    resetBooksGrid()
+    for (let book of library.books){
+        createBookCard(book)
+    }
+}
+
+const resetBooksGrid = () => {
+    booksGrid.innerHTML = ' '
+}
+
+const createBookCard = (book) => {
+    const bookCard = document.createElement     ('div')
+    const title = document.createElement        ('p')
+    const author = document.createElement       ('p')
+    const pages = document.createElement        ('p')
+    const buttonGroup = document.createElement  ('div')
+    const readBtn = document.createElement      ('button')
+    const removeBtn = document.createElement    ('button')
+
+    bookCard.classList.add      ('book-card')
+    buttonGroup.classList.add   ('button-group')
+    readBtn.classList.add       ('btn')
+    removeBtn.classList.add     ('btn')
+    readBtn.onclick = toggleRead
+    removeBtn.onclick = removeBook
+
+    title.textcontent = `"${book.title}"`
+    author.textContent = book.author
+    pages.textContent = `${book.pages} pages`
+    removeBtn.textContent = 'Remove'
+
+    if (book.isRead){
+        readBtn.textContent = 'Read'
+        readBtn.classList.add('btn-light-green')
+    }else{
+        readBtn.textContent = 'Not read'
+        readBtn.classList.add('btn-light-red')
+    }
+
+    bookCard.appendChild    (title)
+    bookCard.appendChild    (author)
+    bookCard.appendChild    (pages)
+    buttonGroup.appendChild (readBtn)
+    buttonGroup.appendChild (removeBtn)
+    bookCard.appendChild    (buttonGroup)
+    booksGrid.appendChild   (bookcard)
+}
+
+const getBookFromInput = () => {
+    const title = document.getElementById('title').value
+    const author = document.getElementById('author').value
+    const pages = document.getElementById('pages').value
+    const isRead = document.getElementById('isRead').value
+    return new Book(title, author, pages, isRead)
+}
+
+const addBook = (e) => {
+    e.preventDefault()
+    const newBook = getBookFromInput()
+
+    if (library.isInLibrary(newBook)){
+        errorMsg.textContent = 'This book already exists in your library'
+        errorMsg.classList.add('active')
+        return
+    }
+
+    if (auth.currentUser){
+        addBookDB(newBook)
+    }else {
+        library.addBook(newBook)
+        saveLocal()
+        updateBooksGrid()
+    }
+
+    closeAddBookModal()
+}
+
+const removeBook = (e) => {
+ const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+    '""',
+    ''
+ )
+
+ if (auth.currentUser){
+    removeBookDB(title)
+ } else {
+    library.removeBook(title)
+    saveLocal()
+    updateBooksGrid()
+ }
+}
+
+const toggleRead = (e) => {
+    const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+        '""',
+        ''
+    )
+    const book = library.getBook(title)
+
+    if (auth.currentUser){
+        toggleBookIsReadDB(book)
+    } else {
+        book.isRead = !book.isRead
+        laveLocal()
+        updateBooksGrid
+    }
+}
+accountBtn.onclick = openAccountModal
+addBookBtn.onclick = openAddBookModal
+overlay.onclick = closeAllModals
+addBookForm.onsubmit = addBook
+window.onkeydown = handleKeyboardInput
+
+//Local Storage
+
+const saveLocal = () => {
+    localStorage.setItem('library', JSON.stringify(library.books))
+}
+
+const restoreLocal = () => {
+    const books = JSON.parse(localStorage.getItem('library'))
+    if (books) {
+        library.books = books.map((book) => JSONToBook(book))
+    } else {
+        library.books = []
+    }
+}
+
+//Auth
+
+const auth = firebase.auth()
+const logInBtn = document.getElementById('logInBtn')
+const logOutBtn = socuemnt.getElementById('logOutBtn')
+
+auth.onAuthStatechanged(async (user) => {
+    if (user) {
+        setupRealTimeListener()
+    } else {
+        if (unsubscribe) unsubscribe()
+        restoreLocal()
+        updateBooksGrid()
+    }
+    setupAccoutModal(user)
+    setupNavbar(user)
+})
+
+const signIn = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+}
+
+const signOut = () => {
+    auth.signOut()
+}
+
+logInBtn.onclick = signIn
+logOutBtn.onclick = signOut
